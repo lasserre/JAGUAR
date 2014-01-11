a9cfe39369a19d86c5ad2ee2356034b5718d796d
John Wilkes <jonathan2@Jonathans-MacBook-Pro.local>
2014-01-11 11:03:34 -0600

    Change MSP430 tests so user can pass in filename to script

--- code/MSP430/test/printFile.py
+++ code/MSP430/test/printFile.py
@@ -1,27 +1,54 @@
 # CopyFileToSerialPort.py
 # -----------------------
-# Description:  Test driver to send contents of FILENAME byte-by-byte out to
+# Description:  Test driver to send contents of filename byte-by-byte out to
 #               a serial port. Intended to be used for testing MSP430 routing
 #               software.
 
 # Config parameters ----------------------
-FILENAME = "ENTER_FILENAME_HERE"
 SERIAL_PORT = "/dev/cu.usbserial-A600eIjc"
 BAUD = 57600
 # ----------------------------------------
 
-import serial
-ser = serial.Serial(SERIAL_PORT, BAUD)
-f = open(FILENAME, "rb")
+import time, serial, sys
+
+# get user inputs
+if (len(sys.argv) < 2):
+	print "Error! A filename must be specified"
+	sys.exit(1)
+filename = sys.argv[1]
+
+if (len(sys.argv) >= 3):
+	loops = int(sys.argv[2])
+else:
+	loops = 1
 
 try:
-    byte = f.read(1)
-    while byte != "":
-        # byte = f.read(1)
-        print str('printing ' + byte)
-        ser.write(byte)
-        byte = f.read(1)
-finally:
-    f.close()
+	ser = serial.Serial(SERIAL_PORT, BAUD)
+except(OSError):
+	print "Error! Could not connect to port \"" + SERIAL_PORT + "\""
+	sys.exit(1)
+
+print "Hex  ASCII"
+print "---  -----"
+
+count = 0
+while loops == 0 or count < loops:
+	f = open(filename, "rb")
+
+	try:
+		byte = f.read(1)
+		while byte != "":
+			# byte = f.read(1)
+			print " " + byte.encode("hex").upper() + (" " * 6) + byte
+			# print "before:", ser.outWaiting()
+			ser.write(byte)
+			# print "after:", ser.outWaiting()
+			# raw_input("")
+			# time.sleep(0.05)
+			byte = f.read(1)
+	finally:
+		f.close()
+
+	count += 1
 
 ser.close()
