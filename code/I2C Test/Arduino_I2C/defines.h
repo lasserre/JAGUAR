129c1ed7e0862bcfa9d30dfabcb70212fe899ae7
Wesley <wefilmer@gmail.com>
2014-01-29 17:22:09 -0600

    Cleaned up files

---
+++ code/I2C Test/Arduino_I2C/defines.h
@@ -0,0 +1,14 @@
+/*
+ * defines.h
+ *
+ *  Created on: Jan 6, 2014
+ *      Author: Jonathan
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+#define I2C_MASTER_ADDRESS  0x08
+#define I2C_SLAVE_ADDRESS   0x09
+
+#endif /* DEFINES_H_ */
