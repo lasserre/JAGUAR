f9411edf01b0b90c5b4ec8b7f8cf53ae5557f13d
John Wilkes <jonathan2@Jonathans-MacBook-Pro.local>
2014-02-01 18:48:57 -0600

    Update Python test script: added delay between messages and a delay for Arduino
    Added a delay after opening the port to allow the Arduino to reset

--- code/MSP430/test/printFile.py
+++ code/MSP430/test/printFile.py
@@ -3,10 +3,12 @@
 # Description:  Test driver to send contents of filename byte-by-byte out to
 #               a serial port. Intended to be used for testing MSP430 routing
 #               software.
-# Command-line args: [filename] [# loops] [delay between bytes]
+# Command-line args: [filename] [# loops] [delay between messages] [delay between bytes]
 
 # Config parameters ----------------------
-SERIAL_PORT = "/dev/cu.usbserial-A600eIjc"
+# SERIAL_PORT = "/dev/cu.usbserial-A600eIjc"
+# SERIAL_PORT = "/dev/cu.usbserial-A40081Ez"
+SERIAL_PORT = "/dev/cu.usbmodemfd121"
 BAUD = 57600
 # ----------------------------------------
 
@@ -18,15 +20,19 @@ if (len(sys.argv) < 2):
 	sys.exit(1)
 filename = sys.argv[1]
 
-delay = 0
+byteDelay = 0
+msgDelay = 0
 loops = 1
-if (len(sys.argv) >= 3):
+if len(sys.argv) >= 3:
 	loops = int(sys.argv[2])
-	if (len(sys.argv) >= 4):
-		delay = float(sys.argv[3])
+if len(sys.argv) >= 4:
+	msgDelay = float(sys.argv[3])
+if len(sys.argv) >= 5:
+	byteDelay = float(sys.argv[4])
 
 try:
 	ser = serial.Serial(SERIAL_PORT, BAUD)
+	# time.sleep(4) # this needs to be uncommented when talking to an Arduino to wait for it to reset :P
 except(OSError):
 	print "Error! Could not connect to port \"" + SERIAL_PORT + "\""
 	sys.exit(1)
@@ -43,15 +49,15 @@ while loops == 0 or count < loops:
 		while byte != "":
 			# byte = f.read(1)
 			print " " + byte.encode("hex").upper() + (" " * 6) + byte
-			# print "before:", ser.outWaiting()
 			ser.write(byte)
-			# print "after:", ser.outWaiting()
 			# raw_input("")
-			time.sleep(delay)
+			time.sleep(byteDelay)
 			byte = f.read(1)
 	finally:
 		f.close()
 
+	f.close()
 	count += 1
+	time.sleep(msgDelay)
 
 ser.close()
