60564a848762716512fefe66a841ff6e745e8a93
John Wilkes <jonathan2@Jonathans-MacBook-Pro.local>
2014-02-07 21:40:16 -0600

    Saving motor-spinning code as an example
    I saved the code to spin the motors as an example in case we ever want
    to revert back to it for testing. Path:
    libraries/AP_Motors/examples/AP_MotorsBlimp_test/AP_MotorsBlimp_test.pde

--- code/Ardupilot/libraries/AP_Motors/AP_MotorsBlimp.cpp
+++ code/Ardupilot/libraries/AP_Motors/AP_MotorsBlimp.cpp
@@ -26,18 +26,18 @@ void AP_MotorsBlimp::setup_motors()
         //Propulsion motors
         //      This is actually two motors (tied together). The 3rd parameter
         //      is 0 because these motors do not affect yaw.
-        add_motor(AP_MOTORS_MOT_1,  0, 0, 2);
+        add_motor(AP_MOTORS_MOT_1,  0, 0, 1);
 
         //Lift motor
-        add_motor(AP_MOTORS_MOT_2, -90, 0, 4);
+        add_motor(AP_MOTORS_MOT_2, -90, 0, 2);
 
 
-        add_motor(AP_MOTORS_MOT_3,   0, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  1);
-        add_motor(AP_MOTORS_MOT_4, 180, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3);
-        add_motor(AP_MOTORS_MOT_5, 180, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3);
-        add_motor(AP_MOTORS_MOT_6, 180, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3);
-        add_motor(AP_MOTORS_MOT_7, 180, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3);
-        add_motor(AP_MOTORS_MOT_8, 180, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3);
+        add_motor(AP_MOTORS_MOT_3,   0, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3);
+        add_motor(AP_MOTORS_MOT_4, 180, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  4);
+        add_motor(AP_MOTORS_MOT_5, 180, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  5);
+        add_motor(AP_MOTORS_MOT_6, 180, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  6);
+        add_motor(AP_MOTORS_MOT_7, 180, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  7);
+        add_motor(AP_MOTORS_MOT_8, 180, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  8);
 
     }
 }
