3dcb81a8b967da4ecff9a4ea8a689a656b57dafc
John Wilkes <jonathan2@Jonathans-MacBook-Pro.local>
2014-04-24 00:17:52 -0500

    XbeeAPILib: Make PayloadStart a const pointer
    Make RxPacket PayloadStart a const pointer

--- code/XbeeAPILib/include/xbee_frame_structs.h
+++ code/XbeeAPILib/include/xbee_frame_structs.h
@@ -73,11 +73,11 @@ struct TxStatus
  */
 struct RxPacket
 {
-    uint64_t SourceAddress;     ///< @brief SourceAddress is the 64-bit address of the sender
-    uint8_t ReceiveOpts;        ///< @brief ReceiveOpts is an 8-bit enum value describing the mode of reception
-    uint8_t* PayloadStart;      ///< @brief PayloadStart points to the start of the payload within the frame supplied
-    uint16_t ExpctdPayloadLen;  ///< @brief ExpctdPayloadLen is the expected payload length based on the LENGTH field
-    uint32_t ActualFrameSize;   ///< @brief ActualFrameSize is the actual frame size detected by parsing
+    uint64_t SourceAddress;      ///< @brief SourceAddress is the 64-bit address of the sender
+    uint8_t ReceiveOpts;         ///< @brief ReceiveOpts is an 8-bit enum value describing the mode of reception
+    const uint8_t* PayloadStart; ///< @brief PayloadStart points to the start of the payload within the frame supplied
+    uint16_t ExpctdPayloadLen;   ///< @brief ExpctdPayloadLen is the expected payload length based on the LENGTH field
+    uint32_t ActualFrameSize;    ///< @brief ActualFrameSize is the actual frame size detected by parsing
 };
 
 /**
